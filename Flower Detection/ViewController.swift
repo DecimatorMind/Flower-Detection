de6f7e5e653e1d5df10db0e7838e61e8154eb601
Pranjal Bhardwaj <E19CSE432@bennett.edu.in>
2020-04-26 20:26:01 +0530

    Add  Oxford102 ML Model to detect flowers.

--- Flower Detection/ViewController.swift
+++ Flower Detection/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        imagePicker.allowsEditing = true
+        
+        imagePicker.sourceType = .camera
+        
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {fatalError("cannot convert to CIImage")}
+            
+            detect(image: ciimage)
+            
+            imageView.image = userPickedImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {fatalError("Cannot import model")}
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        
+            try handler.perform([request])
+            
+        } catch {
+            
+            print(error)
+        }
+        
+        
+    }
+    
 }
 
