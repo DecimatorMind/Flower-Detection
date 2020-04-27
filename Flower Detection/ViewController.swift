f7a566041c507c97bc44b11cb12a492a474ab72b
Pranjal Bhardwaj <E19CSE432@bennett.edu.in>
2020-04-27 23:03:52 +0530

    Add information text using Wiki API

--- Flower Detection/ViewController.swift
+++ Flower Detection/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     
     let imagePicker = UIImagePickerController()
     
@@ -35,8 +42,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             detect(image: ciimage)
             
-            imageView.image = userPickedImage
-            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -56,9 +61,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
             
-            let classification = request.results?.first as? VNClassificationObservation
+            guard let classification = request.results?.first as? VNClassificationObservation else {fatalError("Could not classify image")}
+        
+            self.navigationItem.title = classification.identifier.capitalized
             
-            self.navigationItem.title = classification?.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
             
         }
         
@@ -72,9 +79,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             print(error)
         }
-        
-        
     }
     
+    func requestInfo(flowerName: String) {
+        
+        let parameters : [String:String] = ["format" : "json","action" : "query","prop" : "extracts|pageimages","exintro" : "","explaintext" : "","titles" : flowerName,"indexpageids" : "","redirects" : "1","pithumbsize": "500"]
+
+        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+
+            let dataResponse = response.result
+
+            let flowerJSON: JSON = JSON(dataResponse)
+
+            let pageid = flowerJSON["query"]["pageids"][0].stringValue
+            
+            let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+            
+            let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+            
+            self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+            
+            self.label.text = flowerDescription
+        }
+    }
 }
 
